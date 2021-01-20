2bdde63e10e49b98baa21702405043b99c9edf35
Kristal <kristalkung@gmail.com>
2021-01-20 01:00:29 +0000

    changed printing of sales log from Tuesday to Monday

--- process.py
+++ process.py
@@ -2,15 +2,15 @@ log_file = open("um-server-01.txt")
 
 
 def sales_reports(log_file):
-    """Prints sales reports for Tuesday"""
+    """Prints sales reports for Monday"""
     for line in log_file:
     # loop through each item in log_file
         line = line.rstrip()
         # removes trailing characters at the end of each line
         day = line[0:3]
         # sets variable day as the first 3 characters of line
-        if day == "Tue":
-        # if the day is Tuesday
+        if day == "Mon":
+        # if the day is Monday
             print(line)
             #prints the log
 
